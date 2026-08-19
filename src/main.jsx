9ac90ec5dca42199d5c41cfcea91f61e7afed42b
Harshit Gujarathi <HARSHIT.GUJRATHI67@svkmmumbai.onmicrosoft.com>
2026-08-19 13:40:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef } from "react";
+ import React, { useLayoutEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
